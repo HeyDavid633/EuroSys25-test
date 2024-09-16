65ab3833d12bf8c100db9406ebd3f2b41dad2cd4
HeyDavid633 <wenhao.dai633@gmail.com>
2024-09-16 10:00:34 +0800

    ready for A100 test

--- attn-compile-print.py
+++ attn-compile-print.py
@@ -1,15 +1,16 @@
 # 2024.9.16 Mon. 
 # 期望打印出 Torch.compile 在不同模式下的融合信息
 # Torch.compile(Triton) 
+# 信息打印专用，故不再多次运行；而且不需要base和计时
 # 
-# python attn-compile-mode.py 
+# python attn-compile-print.py 
 
-import timeit
 import torch
 import numpy as np
 import random
 import torch.nn.functional as F
 from utils.utils import *
+import torch._dynamo
 import config
 
 @torch.jit.script
@@ -38,7 +39,6 @@ def attn_example():
     
     torch_cuda_identify()
     
-    avg_seq_len = config.AVG_SEQ_LEN
     batch_size = config.BATCH_SIZE
     head_size = config.HEAD_DIM # head_dim aka. head_size
     seq_len = config.SEQ_LEN
@@ -46,22 +46,14 @@ def attn_example():
     layer_num = config.LAYER_NUM
     hidden_dim = head_num * head_size
     
-    warmup_iters = config.WARMUP_TIME
-    running_iters = config.RUNNING_TIME
+    # warmup_iters = config.WARMUP_TIME
+    # running_iters = config.RUNNING_TIME
     dtype = "fp16"
 
-
-    if avg_seq_len <= 0:
-        avg_seq_len = seq_len
-    low, high = (2 * avg_seq_len - seq_len, seq_len + 1) if 2 * avg_seq_len > seq_len else (0, 2 * avg_seq_len + 1)
-    input_lens = torch.randint(low=low, high=high, size=(batch_size,))
-    seqlen_mask = seqlen_to_mask(input_lens, seq_len)
-    
     input_from_tensor           = set_dtype(torch.empty(batch_size, seq_len, hidden_dim).uniform_(-0.4, 0.4).cuda(), dtype)
     qkv_kernel                  = [set_dtype(torch.zeros(hidden_dim, hidden_dim * 3).uniform_(-0.4, 0.4).cuda(), dtype) for _ in range(layer_num)]
     qkv_bias                    = [set_dtype(torch.zeros(hidden_dim * 3).uniform_(-0.4, 0.4).cuda(), dtype) for _ in range(layer_num)]
-    
-        
+      
     hidden_states = input_from_tensor
     layer = 0 
     qkv = torch.matmul(hidden_states, qkv_kernel[layer]) + qkv_bias[layer]
@@ -70,45 +62,15 @@ def attn_example():
     k = transpose_for_scores(k, head_num, head_size)
     v = transpose_for_scores(v, head_num, head_size)
 
+    
+    
+    hidden_states1 = Attention_Jit(q, k, v) 
 
-    for i in range(warmup_iters + running_iters):
-        if i == warmup_iters:    
-            torch.cuda.synchronize()
-            t0_start = timeit.default_timer()                   
-        scores = torch.matmul(q, k.transpose(-2, -1)) / (head_size ** .5)
-        probs = F.softmax(scores, dim=-1)
-        h = torch.matmul(probs, v).permute(0, 2, 1, 3).contiguous() 
-        new_context_layer_shape = h.size()[:-2] + (hidden_dim, )
-        hidden_states0 = h.view(new_context_layer_shape)    
-    torch.cuda.synchronize()
-    t0_end = timeit.default_timer()
     
-    base_time = (t0_end - t0_start) * 1000 / running_iters
-    print("bs:{} | seq:{} | Base   : {:.3f} ms / iter".format(batch_size, seq_len, base_time))  
+    hidden_states2 = Attention_Compile(q, k, v) 
     
-    for i in range(warmup_iters + running_iters):
-        if i == warmup_iters:    
-            torch.cuda.synchronize()
-            t1_start = timeit.default_timer()   
-        hidden_states1 = Attention_Jit(q, k, v) 
-    torch.cuda.synchronize()
-    t1_end = timeit.default_timer()
     
-    jit_time = (t1_end - t1_start) * 1000 / running_iters
-    print("bs:{} | seq:{} | Jit    : {:.3f} ms / iter".format(batch_size, seq_len, jit_time))  
     
-    for i in range(warmup_iters + running_iters):
-        if i == warmup_iters:    
-            torch.cuda.synchronize()
-            t2_start = timeit.default_timer()
-        hidden_states2 = Attention_Compile(q, k, v) 
-    torch.cuda.synchronize()
-    t2_end = timeit.default_timer()
-    
-    torch_compile_time = (t2_end - t2_start) * 1000 / running_iters
-    print("bs:{} | seq:{} | compile: {:.3f} ms / iter\n".format(batch_size, seq_len, torch_compile_time))  
-
-                
                             
 if __name__ == '__main__':
         
